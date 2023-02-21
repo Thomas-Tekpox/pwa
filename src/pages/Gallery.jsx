44a4196625b7e87dd8b68b6770477d45c76d2b55
louis <louis@huort.com>
2023-02-21 16:00:36 +0100

    wow

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -46,7 +46,6 @@ const Gallery = () => {
             src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
             srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
             alt={item.title}
-            loading="lazy"
             key={item.img}
           />
         ))}
